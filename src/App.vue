496a4f2bc8e4ec986251da1bcc620a2c51147c74
Mohammad <sargazi1377@gmail.com>
2020-04-27 00:03:39 -0500

    Would-you-rather final

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+<div id="app">
+  <whouldYouRatherQestion
+  v-bind:questions="questions"
+  v-on:answer-changed="userAnswers">
+  </whouldYouRatherQestion>
+  <div class="result"> 
+      <h1>{{  ResultHeader }}</h1>
+  </div>
+   <div class="answers"> 
+    <p>
+     
+      <ul>
+        <li v-for="choice in userChoices" :key="choice"><span>{{choice}}</span></li>                
+      </ul>
+    </p>
+    </div>
+
+</div>
+</template>
+
+<script>
+import whouldYouRatherQestion from './components/wouldYouRatherQuestion.vue'
+export default {
+    name:'App',
+    components:{
+      whouldYouRatherQestion
+    },
+    data() {
+        return {
+        ResultHeader: 'You Would Rather...',
+        userChoices: [],    
+        questions: [
+        {
+            id:0,
+            question :'Would you rather live in a house shaped like a circle or a house shaped like a triangle?',
+            answer1: 'Circle',
+            answer2: 'Triangle'
+        },
+        {
+            id:1,
+            question :'Would you rather be able to type/text very fast or be able to read really quickly?',
+            answer1: 'Type/text very fast',
+            answer2: 'Read really quickly'
+        },
+        {
+            id:2,
+            question :'Would you rather ride a very big horse or a very small pony?',
+            answer1: 'Big horse',
+            answer2: 'Small pony'
+        }
+        ]
+
+
+    }
+},
+methods: {
+    userAnswers(choice) {
+        // console.log(choice)
+        var pieces = choice.split(","); // split choice by , to get the id for each answer 
+        var index=pieces[1];
+        this.$set(this.userChoices, pieces[1], pieces[0])    // will be reactive      
+        // this.$set(this.userChoices, 0, ID)    // will be reactive      
+      
+    
+    }
+}
+                
+}
+</script>
+
+<style>
+.result {
+    text-align: center;
+}
+.answers {
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 500;
+    font-variant: small-caps;
+    font-size: 25px;
+    color: rgb(128, 86, 71);
+     line-height: 1.6;
+    
+
+}
+
+</style>
+
+
---
+++ src/components/wouldYouRatherQuestion.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <div class="container"
+         v-for=" i in questions" :key="i.id">         
+        <h2> {{ i.question }}</h2>
+        <input id=button name="i.id" type="radio" v-model= "choice" v-bind:value="i.answer1+','+i.id"  v-on:change="$emit('answer-changed', choice)">        
+         <label> {{ i.answer1 }}</label>
+         <input id=button  name=i.id type="radio" v-model= "choice" v-bind:value="i.answer2+','+i.id" v-on:change="$emit('answer-changed', choice)">
+         <label> {{ i.answer2 }}</label>         
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name:'wouldYouRatherQuestion',
+props:{
+    questions: Array
+},
+    data() {
+        return {
+            choice:""
+        }
+    },
+} 
+</script>
+
+<style>
+body{
+    background-color: rgb(218, 181, 221);
+}
+.container {
+    background-color: rgb(209, 181, 236);
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    text-align:  center;
+    font-size: 20px;
+
+}
+#button{
+    font-family: 'Times New Roman', Times, serif;
+}
+
+</style>
+
+
